39ac302a1ecd540702cd2c1128c266cfe7f49c9c
Igor Zashchelkin <igorzash@tutanota.com>
2021-09-10 15:50:34 +0300

    Add Cache middleware

--- app.py
+++ app.py
@@ -8,8 +8,10 @@ from resources.distribution.regions import RegionsResource
 from resources.distribution.data_types import DataTypesResource
 from resources.distribution.summary import SummaryResource
 
+from middleware.cache import Cache
 
-app = falcon.asgi.App(cors_enable=True)
+
+app = falcon.asgi.App(cors_enable=True, middleware=[Cache()])
 
 
 app.add_route('/api/v1/distribution/regions', RegionsResource())
---
+++ middleware/cache.py
@@ -0,0 +1,38 @@
+from datetime import datetime, timedelta
+
+
+class Cache:
+    CACHE_HEADER = 'X-API-Cache'
+    TTL = 60 * 60 * 12
+
+    store = dict()
+    cache_record = dict()
+
+    async def process_request(self, req, resp):
+        try:
+            resp.context.cached = False
+
+            data = self.store.get(req.path)
+            expire_date = self.cache_record.get(req.path)
+
+
+            if data is not None and expire_date is not None and datetime.now() < expire_date:
+                resp.text = data
+                resp.set_header(self.CACHE_HEADER, 'Hit')
+                resp.context.cached = True
+            else:
+                resp.set_header(self.CACHE_HEADER, 'Miss')
+        except Exception as e:
+            print(e)
+
+
+    async def process_response(self, req, resp, resource, req_succeeded):
+        try:
+            if not req_succeeded:
+                return
+
+            if not resp.context.cached:
+                self.store[req.path] = resp.text
+                self.cache_record[req.path] = datetime.now() + timedelta(seconds=self.TTL)
+        except Exception as e:
+            print(e)
